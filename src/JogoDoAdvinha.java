8da20cf5a3c73975448ea3f9950c24d6f78e7fd0
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-12 14:30:30 -0300

    feat(jogo): adiciona estrutura inicial do Jogo do Adivinha
    Incluida a estrutura básica do jogo, com uso dde Scanner para entrada do usuário e Ramdom para geração de números.

---
+++ src/JogoDoAdvinha.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogoDoAdvinha {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+
+
+    }
+}
