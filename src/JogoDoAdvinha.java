212afcb60e896f29de8b8e245b9aed1f75c0c098
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-12 14:55:28 -0300

    feat(jogo): implementa lógica inicial do loop principal
    Adicionando o loop principal do jogo com controle de continuidade, geração de número aleatório, contador de tentativas e verificação de acerto

--- src/JogoDoAdvinha.java
+++ src/JogoDoAdvinha.java
@@ -6,6 +6,20 @@ public class JogoDoAdvinha {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
+        int pontuacaoTotal = 0;
+        boolean continuar = true;
+
+        System.out.println("Bᴇᴍ-ᴠɪɴᴅᴏ ᴀᴏ Jᴏɢᴏ ᴅᴏ Aᴅɪᴠɪɴʜᴀ");
+
+        while (continuar) {
+            int numeroSorteado = random.nextInt(100) + 1;
+            int tentativas = 0;
+            boolean acertou = false;
+
+            System.out.println("Tᴇɴᴛᴇ ᴀᴅɪᴠɪɴʜᴀ ᴏ ɴᴜ́ᴍᴇʀᴏ (ᴇɴᴛʀᴇ 1 ᴇ 100): ");
+
+        }
+
 
 
     }
